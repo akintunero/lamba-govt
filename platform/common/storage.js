7daf0c51238ccc3e89c5c98434b6af68756459be
Olumayowa Akinkuehinmi <akintunero101@gmail.com>
2025-07-15 10:09:28 +0100

    feat: add object storage utilities

---
+++ platform/common/storage.js
@@ -0,0 +1,40 @@
+const { serviceFetch } = require('./http');
+
+const STORAGE_URL = process.env.FILE_STORAGE_SERVICE_URL || 'http://file-storage-service:3007';
+
+async function uploadObject({ bucket, filename, content, mimeType, citizenId, documentId, correlationId }) {
+  return serviceFetch(STORAGE_URL, '/v1/objects/upload', {
+    method: 'POST',
+    correlationId,
+    body: { bucket, filename, content, mimeType, citizenId, documentId }
+  });
+}
+
+async function getSignedUrl({ bucket, objectKey, expirySeconds, correlationId }) {
+  return serviceFetch(STORAGE_URL, `/v1/objects/${bucket}/${encodeURIComponent(objectKey)}/signed-url?expiry=${expirySeconds || 3600}`, {
+    correlationId
+  });
+}
+
+async function getObjectMetadata({ bucket, objectKey, correlationId }) {
+  return serviceFetch(STORAGE_URL, `/v1/objects/${bucket}/${encodeURIComponent(objectKey)}`, { correlationId });
+}
+
+async function archiveObject({ bucket, objectKey, correlationId }) {
+  return serviceFetch(STORAGE_URL, `/v1/objects/${bucket}/${encodeURIComponent(objectKey)}/archive`, {
+    method: 'POST',
+    correlationId
+  });
+}
+
+async function getCitizenHistory(citizenId, correlationId) {
+  return serviceFetch(STORAGE_URL, `/v1/objects/citizen/${citizenId}/history`, { correlationId });
+}
+
+module.exports = {
+  uploadObject,
+  getSignedUrl,
+  getObjectMetadata,
+  archiveObject,
+  getCitizenHistory
+};
