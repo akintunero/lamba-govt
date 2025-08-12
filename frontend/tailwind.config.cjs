47b915932050b5df730be3a6e8586e4fc4f35274
Olumayowa Akinkuehinmi <akintunero101@gmail.com>
2025-08-12 14:27:39 +0100

    chore: bootstrap frontend toolchain

---
+++ frontend/tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        govblue: '#0033A0'
+      }
+    }
+  },
+  plugins: []
+};
+
